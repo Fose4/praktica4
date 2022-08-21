859d4f51859d0189d0e9c68bbb36484992705393
Artem <styoganov@bk.ru>
2022-08-21 12:34:01 +0300

    Commit 1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+int NumbersOfDigit(int num)
+{
+    while (num > 0)
+    {
+        count++;
+        num = num / 10 ;
+    }
+    return count;
+}
+Console.WriteLine($"{NumbersOfDigit(number)}");
